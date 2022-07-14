a7a6b2f472ebf510db37a392fadc31aecf09057c
Antonina Nikolaeva <minimalanny@gmail.com>
2022-07-14 21:59:03 +0500

    HW7

---
+++ HomeWork7-1/HomeWork7-1/Main.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace HomeWork7_1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMulty = new System.Windows.Forms.Button();
+            this.labelComputerNumber = new System.Windows.Forms.Label();
+            this.labelUserNumber = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.buttonUdvo = new System.Windows.Forms.Button();
+            this.buttonUgad = new System.Windows.Forms.Button();
+            this.textBoxUserNumber = new System.Windows.Forms.TextBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.labelAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonReset
+            // 
+            resources.ApplyResources(this.buttonReset, "buttonReset");
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonPlus
+            // 
+            resources.ApplyResources(this.buttonPlus, "buttonPlus");
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMulty
+            // 
+            resources.ApplyResources(this.buttonMulty, "buttonMulty");
+            this.buttonMulty.Name = "buttonMulty";
+            this.buttonMulty.UseVisualStyleBackColor = true;
+            this.buttonMulty.Click += new System.EventHandler(this.buttonMulty_Click);
+            // 
+            // labelComputerNumber
+            // 
+            resources.ApplyResources(this.labelComputerNumber, "labelComputerNumber");
+            this.labelComputerNumber.Name = "labelComputerNumber";
+            // 
+            // labelUserNumber
+            // 
+            resources.ApplyResources(this.labelUserNumber, "labelUserNumber");
+            this.labelUserNumber.Name = "labelUserNumber";
+            // 
+            // labelCount
+            // 
+            resources.ApplyResources(this.labelCount, "labelCount");
+            this.labelCount.Name = "labelCount";
+            // 
+            // buttonUdvo
+            // 
+            resources.ApplyResources(this.buttonUdvo, "buttonUdvo");
+            this.buttonUdvo.Name = "buttonUdvo";
+            this.buttonUdvo.UseVisualStyleBackColor = true;
+            this.buttonUdvo.Click += new System.EventHandler(this.buttonUdvo_Click);
+            // 
+            // buttonUgad
+            // 
+            resources.ApplyResources(this.buttonUgad, "buttonUgad");
+            this.buttonUgad.Name = "buttonUgad";
+            this.buttonUgad.UseVisualStyleBackColor = true;
+            this.buttonUgad.Click += new System.EventHandler(this.buttonUgad_Click);
+            // 
+            // textBoxUserNumber
+            // 
+            resources.ApplyResources(this.textBoxUserNumber, "textBoxUserNumber");
+            this.textBoxUserNumber.Name = "textBoxUserNumber";
+            // 
+            // labelInfo
+            // 
+            resources.ApplyResources(this.labelInfo, "labelInfo");
+            this.labelInfo.Name = "labelInfo";
+            // 
+            // buttonOk
+            // 
+            resources.ApplyResources(this.buttonOk, "buttonOk");
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // labelAnswer
+            // 
+            resources.ApplyResources(this.labelAnswer, "labelAnswer");
+            this.labelAnswer.Name = "labelAnswer";
+            // 
+            // Main
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelAnswer);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.textBoxUserNumber);
+            this.Controls.Add(this.buttonUgad);
+            this.Controls.Add(this.buttonUdvo);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.labelUserNumber);
+            this.Controls.Add(this.labelComputerNumber);
+            this.Controls.Add(this.buttonMulty);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.buttonReset);
+            this.Name = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMulty;
+        private System.Windows.Forms.Label labelComputerNumber;
+        private System.Windows.Forms.Label labelUserNumber;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Button buttonUdvo;
+        private System.Windows.Forms.Button buttonUgad;
+        private System.Windows.Forms.TextBox textBoxUserNumber;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Label labelAnswer;
+    }
+}
+
---
+++ HomeWork7-1/HomeWork7-1/Main.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork7_1
+{
+    public partial class Main : Form
+    {
+        public Random random = new Random();
+        private int computerNumber;
+        private int userNumber;
+        private int count;
+
+        public Main()
+        {
+            InitializeComponent();
+            UpdateState(userNumber, random.Next(20));
+            count = 0;
+            UpdateCount(count);
+        }
+        private void UpdateState(int userNamber)
+        {
+            labelUserNumber.Text = $"Текущее число: {userNumber}";
+        }
+
+        private void UpdateState(int userNamber, int computerNumber)
+        {
+            UpdateState(userNumber);
+            this.computerNumber = computerNumber;
+            labelComputerNumber.Text = $"Получите число: {computerNumber}";
+        }
+
+        private void UpdateCount(int Count)
+        {
+            labelCount.Text = $"Ходов: {count}";
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            userNumber = 0;
+            UpdateState(userNumber, random.Next(20));
+            count = 0;
+            UpdateCount(count);
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            UpdateState(++userNumber);
+            UpdateCount(++count);
+            СheckWin();
+        }
+
+        private void buttonMulty_Click(object sender, EventArgs e)
+        {
+            UpdateState(userNumber *=2);
+            UpdateCount(++count);
+            СheckWin();
+        }
+
+        private void СheckWin()
+        {
+            if (userNumber == computerNumber)
+            {
+                MessageBox.Show($"Вы успешно завершили игру за {count} ходов", "Две игры", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void СheckNumber()
+        {
+            if (userNumber > computerNumber)
+            {
+                MessageBox.Show("Ваше число больше загаданного", "Две игры", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (userNumber > computerNumber)
+            {
+                MessageBox.Show("Ваше число меньше загаданного", "Две игры", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (userNumber == computerNumber)
+            {
+                СheckWin();
+            }
+
+        }
+
+        private void buttonUdvo_Click(object sender, EventArgs e)
+        {
+
+            buttonPlus.Visible = true;
+            buttonMulty.Visible = true;
+            labelComputerNumber.Visible = true;
+            labelUserNumber.Visible = true;
+            labelCount.Visible = true;
+            buttonReset.Visible = true;
+            labelAnswer.Visible = true;
+
+
+
+            textBoxUserNumber.Visible = false;
+            buttonOk.Visible = false;
+
+        }
+
+        private void buttonUgad_Click(object sender, EventArgs e)
+        {
+        labelComputerNumber.Visible = true;
+        labelUserNumber.Visible = true;
+        textBoxUserNumber.Visible = true;
+        buttonOk.Visible = true;
+            labelCount.Visible = true;
+
+            buttonReset.Visible = true;
+
+            buttonPlus.Visible = false;
+            buttonMulty.Visible = false;
+            labelAnswer.Visible = true;
+
+
+
+
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            userNumber = Convert.ToInt32(textBoxUserNumber.Text);
+            UpdateCount(++count);
+            СheckNumber();
+            //textBoxUserNumber.Text = "";
+        }
+    }
+}
