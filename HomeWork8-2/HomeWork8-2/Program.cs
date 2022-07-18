2fc2b07e619fa17e2eba8079aac964eba20c605f
Antonina Nikolaeva <minimalanny@gmail.com>
2022-07-18 21:58:39 +0500

    hw8

---
+++ HomeWork8-2/HomeWork8-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+class Program
+{
+    static PropertyInfo GetPropertyInfo(object obj, string str)
+    {
+        return obj.GetType().GetProperty(str);
+    }
+
+
+    static void Main(string[] args)
+    {
+
+    DateTime dT1 = new DateTime();
+        //dateTime.DayOfWeek
+        //Console.WriteLine(GetPropertyInfo(dT1, "DayOfWeek").CanRead);
+        //Console.WriteLine(GetPropertyInfo(dT1, "DayOfWeek").CanWrite);
+        Console.WriteLine(GetPropertyInfo(dT1, "DayOfWeek").GetValue(dT1, null));
+        Console.WriteLine(GetPropertyInfo(dT1, "DayOfWeek").GetValue(dT1, null));
+
+        Type type = typeof(DateTime);
+        foreach (var prop in type.GetProperties())
+            Console.WriteLine(prop.Name);
+
+
+
+
+
+        Console.ReadKey();
+    }
+}
